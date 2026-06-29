ceb06b23bcf9df3013d64339bdaa6d206d7e82c8
JUNHYEONG LEE <nibble1492@gmail.com>
2026-06-29 11:08:03 +0900

    feat: ChatRoom 엔티티 및 Status 열거형(Enum) 구현

--- back/src/main/java/com/back/domain/chat/chatRoom/entity/ChatRoom.java
+++ back/src/main/java/com/back/domain/chat/chatRoom/entity/ChatRoom.java
@@ -1,4 +1,35 @@
 package com.back.domain.chat.chatRoom.entity;
 
-public class ChatRoom {
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ChatRoom extends BaseEntity {
+
+    @Enumerated(EnumType.STRING)
+    private ChatRoomStatus status;
+
+    @Column(name = "max_participants")
+    private int maxParticipants;
+
+    private LocalDateTime closedAt;
+
+    public ChatRoom(ChatRoomStatus status, int maxParticipants) {
+        this.status = status;
+        this.maxParticipants = maxParticipants;
+    }
+
+    public void close() {
+        this.status = ChatRoomStatus.CLOSED;
+        this.closedAt = LocalDateTime.now();
+    }
 }
---
+++ back/src/main/java/com/back/domain/chat/chatRoom/entity/ChatRoomStatus.java
@@ -0,0 +1,6 @@
+package com.back.domain.chat.chatRoom.entity;
+
+public enum ChatRoomStatus {
+    ACTIVE,
+    CLOSED
+}
