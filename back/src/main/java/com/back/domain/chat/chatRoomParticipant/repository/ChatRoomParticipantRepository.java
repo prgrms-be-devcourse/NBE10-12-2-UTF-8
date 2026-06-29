89faa5f6a747c1a34f0b4e1506f0be7c2a68e81c
jjun012 <163534242+jjun012@users.noreply.github.com>
2026-06-29 11:43:15 +0900

    feat: ChatRoomParticipant Repository 구현 (#40)

--- back/src/main/java/com/back/domain/chat/chatRoomParticipant/repository/ChatRoomParticipantRepository.java
+++ back/src/main/java/com/back/domain/chat/chatRoomParticipant/repository/ChatRoomParticipantRepository.java
@@ -1,4 +1,9 @@
 package com.back.domain.chat.chatRoomParticipant.repository;
 
-public interface ChatRoomParticipantRepository {
+import com.back.domain.chat.chatRoomParticipant.entity.ChatRoomParticipant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ChatRoomParticipantRepository extends JpaRepository<ChatRoomParticipant, UUID> {
 }
