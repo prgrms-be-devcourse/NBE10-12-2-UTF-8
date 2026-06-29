9da36a357cda442b546c2b447a6678dde1f01bb3
최성혁 <joysys8272@gmail.com>
2026-06-29 14:02:17 +0900

    feat: ChatMessageRepository 구현 (#11)

--- back/src/main/java/com/back/domain/chat/chatRoomMessage/repository/ChatMessageRepository.java
+++ back/src/main/java/com/back/domain/chat/chatRoomMessage/repository/ChatMessageRepository.java
@@ -1,4 +1,9 @@
 package com.back.domain.chat.chatRoomMessage.repository;
 
-public interface ChatMessageRepository {
+import com.back.domain.chat.chatRoomMessage.entity.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, UUID> {
 }
