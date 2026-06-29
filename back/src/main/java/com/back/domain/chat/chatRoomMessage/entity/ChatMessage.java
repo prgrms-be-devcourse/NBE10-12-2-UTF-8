54cd105462c3b33715cd11f310b72ec489075944
hyeok314 <joysys8272@gmail.com>
2026-06-29 11:40:16 +0900

    feat: ChatRoomMessage Entity 추가

--- back/src/main/java/com/back/domain/chat/chatRoomMessage/entity/ChatMessage.java
+++ back/src/main/java/com/back/domain/chat/chatRoomMessage/entity/ChatMessage.java
@@ -1,4 +1,38 @@
 package com.back.domain.chat.chatRoomMessage.entity;
 
-public class ChatMessage {
+import com.back.domain.chat.chatRoom.entity.ChatRoom;
+import com.back.domain.chat.chatRoomParticipant.entity.ChatRoomParticipant;
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ChatMessage extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "participant_id", nullable = false)
+    private ChatRoomParticipant participant;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public  ChatMessage(ChatRoom chatRoom, ChatRoomParticipant participant, String content) {
+        this.chatRoom = chatRoom;
+        this.participant = participant;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = LocalDateTime.now();
+    }
+
 }
