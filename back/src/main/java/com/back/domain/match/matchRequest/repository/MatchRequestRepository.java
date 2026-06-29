20c94c7b672b6894ab2025232354c41812925318
leesm-225 <lshine12393@gmail.com>
2026-06-29 14:16:45 +0900

    MatchRequestRepository, MatchRequestEntity 수정

--- back/src/main/java/com/back/domain/match/matchRequest/repository/MatchRequestRepository.java
+++ back/src/main/java/com/back/domain/match/matchRequest/repository/MatchRequestRepository.java
@@ -1,8 +1,20 @@
 package com.back.domain.match.matchRequest.repository;
 
 import com.back.domain.match.matchRequest.entity.MatchRequest;
+import com.back.domain.match.matchRequest.entity.MatchStatus;
+import com.back.domain.member.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MatchRequestRepository extends JpaRepository<MatchRequest, Integer> {
+import java.util.Optional;
+import java.util.UUID;
+
+public interface MatchRequestRepository extends JpaRepository<MatchRequest, UUID> {
+    boolean existsByMemberAndStatus(Member member, MatchStatus status);
+    Optional<MatchRequest> findFirstByMember_IndustryAndSituationAndStatus(
+            String industry, String situation, MatchStatus status);
+    Optional<MatchRequest> findFirstByMember_IndustryAndStatus(
+            String industry, MatchStatus status);
+
+
 
 }
