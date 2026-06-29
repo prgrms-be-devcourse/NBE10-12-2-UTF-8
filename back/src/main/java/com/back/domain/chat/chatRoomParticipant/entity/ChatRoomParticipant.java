1fe07960433697ec6a48be69f5e1a9ceac45963d
jjun012 <163534242+jjun012@users.noreply.github.com>
2026-06-29 10:49:32 +0900

    feat: ChatRoomParticipant 엔티티 구현 (#34)

--- back/src/main/java/com/back/domain/chat/chatRoomParticipant/entity/ChatRoomParticipant.java
+++ back/src/main/java/com/back/domain/chat/chatRoomParticipant/entity/ChatRoomParticipant.java
@@ -1,4 +1,35 @@
 package com.back.domain.chat.chatRoomParticipant.entity;
 
-public class ChatRoomParticipant {
+import com.back.domain.chat.chatRoom.entity.ChatRoom;
+import com.back.domain.member.member.entity.Member;
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ChatRoomParticipant extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    private String nickname;
+
+    private LocalDateTime joinedAt;
+
+    public ChatRoomParticipant(ChatRoom chatRoom, Member member, String nickname) {
+        this.chatRoom = chatRoom;
+        this.member = member;
+        this.nickname = nickname;
+        this.joinedAt = LocalDateTime.now();
+    }
 }
