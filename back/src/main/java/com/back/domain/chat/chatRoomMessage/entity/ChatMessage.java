0543cdc8c904355bba1ed2db6e9e57fc7cb5dc0e
최성혁 <joysys8272@gmail.com>
2026-06-29 12:40:13 +0900

    feat: ChatRoomMessage Entity 추가 (#44)
    * feat: ChatRoomMessage Entity 추가
    
    * fix: ChatRoomMessageEntity에서 createdAt, modifiedAt 생략

--- back/src/main/java/com/back/domain/chat/chatRoomMessage/entity/ChatMessage.java
+++ back/src/main/java/com/back/domain/chat/chatRoomMessage/entity/ChatMessage.java
@@ -1,4 +1,34 @@
 package com.back.domain.chat.chatRoomMessage.entity;
 
-public class ChatMessage {
+import com.back.domain.chat.chatRoom.entity.ChatRoom;
+import com.back.domain.chat.chatRoomParticipant.entity.ChatRoomParticipant;
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ChatMessage extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "participant_id", nullable = false)
+    private ChatRoomParticipant participant;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+
+    public  ChatMessage(ChatRoom chatRoom, ChatRoomParticipant participant, String content) {
+        this.chatRoom = chatRoom;
+        this.participant = participant;
+        this.content = content;
+    }
+
 }
