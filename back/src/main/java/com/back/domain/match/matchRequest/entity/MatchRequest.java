a4ca4aa1c07f93f9caceacf08da5893f6dd5227b
leesm-225 <lshine12393@gmail.com>
2026-06-29 11:24:33 +0900

    수정2

--- back/src/main/java/com/back/domain/match/matchRequest/entity/MatchRequest.java
+++ back/src/main/java/com/back/domain/match/matchRequest/entity/MatchRequest.java
@@ -1,42 +1,36 @@
 package com.back.domain.match.matchRequest.entity;
 
+import com.back.domain.chat.chatRoom.entity.ChatRoom;
 import com.back.domain.member.member.entity.Member;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 @Entity
 @Getter
 @NoArgsConstructor
 public class MatchRequest extends BaseEntity {
 
-    @Id
-    private UUID matchRequestId;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    private String roomId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private ChatRoom room;
+
     private String industry;
     private String situation;
 
     @Enumerated(EnumType.STRING)
     private MatchStatus status;
 
-    private LocalDateTime requestedAt;
-
-    public MatchRequest(Member member, String roomId, String industry, String situation) {
-        this.matchRequestId = UUID.randomUUID();
+    public MatchRequest(Member member, ChatRoom room, String industry, String situation) {
         this.member = member;
-        this.roomId = roomId;
+        this.room = room;
         this.industry = industry;
         this.situation = situation;
         this.status = MatchStatus.PENDING;
-        this.requestedAt = requestedAt;
     }
 }
--- back/src/main/java/com/back/domain/match/matchRequest/entity/MatchStatus.java
+++ back/src/main/java/com/back/domain/match/matchRequest/entity/MatchStatus.java
@@ -1,8 +1,6 @@
 package com.back.domain.match.matchRequest.entity;
 
 public enum MatchStatus {
-    PENDING,
-    //매칭 대기중
-    MATCHED
-    //매칭완료
+    PENDING,  // 매칭 대기중
+    MATCHED   // 매칭 완료
 }
