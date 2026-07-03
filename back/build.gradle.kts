b92ee43aa5be09144f6918ef5696c4dce41c31a9
leesm-225 <lshine12393@gmail.com>
2026-07-03 16:51:20 +0900

    rebase

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
     // H2 Console
     implementation("org.springframework.boot:spring-boot-h2console")
